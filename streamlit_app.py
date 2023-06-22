1725f0333dfef5972945ba596e43c600ebbca32e
grant price <gprice@usc.edu>
2023-06-22 13:06:01 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,10 +7,10 @@ with st.sidebar:
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-st.title("💬 Streamlit GPT")
+st.title("💬 GPT Genie")
 openai_api_key = st.secrets["chatbot_api_key"]
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    st.session_state["messages"] = [{"role": "assistant", "content": "Tell me about yourself"}]
 
 with st.form("chat_input", clear_on_submit=True):
     a, b = st.columns([4, 1])
